8c1adb99258a5d5b4c65ced8d8fd462b550eea00
Hady-7 <83098591+Hady-7@users.noreply.github.com>
2021-11-08 17:03:57 +0200

    starting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoctorFormComponent } from './DoctorForm/doctor-form/doctor-form.component';
+import { HeaderComponent } from './Layout/header/header.component';
+import { FooterComponent } from './Layout/footer/footer.component';
+import { FindDoctorComponent } from './HomeScreen/find-doctor/find-doctor.component';
+import { CarouselComponent } from './HomeScreen/carousel/carousel.component';
+import { StaticContentComponent } from './HomeScreen/static-content/static-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorFormComponent,
+    HeaderComponent,
+    FooterComponent,
+    FindDoctorComponent,
+    CarouselComponent,
+    StaticContentComponent
   ],
   imports: [
     BrowserModule,

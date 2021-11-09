80594f5c31fef83ccc678126c7993b275877e02a
Hady-7 <83098591+Hady-7@users.noreply.github.com>
2021-11-09 22:44:55 +0200

    new deb

--- src/app/DoctorForm/doctor-form/doctor-form.component.ts
+++ src/app/DoctorForm/doctor-form/doctor-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-doctor-form',
@@ -11,5 +12,7 @@ export class DoctorFormComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  onSubmit(Form:NgForm){
+    console.log(Form.value);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDxtLeqwR3AMlQYM6zk7Uz4e8_Dk0MZWoI",
+    authDomain: "clinido-ce15d.firebaseapp.com",
+    projectId: "clinido-ce15d",
+    storageBucket: "clinido-ce15d.appspot.com",
+    messagingSenderId: "584753027934",
+    appId: "1:584753027934:web:719e525a3580d8fbad8dcf"
+  }
 };
 
 /*

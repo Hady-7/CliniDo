df4dc6883dab69a2b00917a440b883bfdf2ea045
hafsa ahmed <sooahmed83@yahoo.com>
2021-11-12 21:55:09 +0200

    third update

--- src/app/HomeScreen/find-doctor/find-doctor.component.ts
+++ src/app/HomeScreen/find-doctor/find-doctor.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Doctor } from 'src/app/models/Doctor.model';
+import { DrCategory } from 'src/app/models/DrCategory.model';
+import { DrCity } from 'src/app/models/DrCity.model';
+import { CategoryService } from 'src/app/services/category.service';
+import { CityCatService } from './../../services/city-cat.service';
 
 @Component({
   selector: 'app-find-doctor',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./find-doctor.component.scss']
 })
 export class FindDoctorComponent implements OnInit {
+  DoctorForm=<Doctor>{};
+  DoctorCategory!:DrCategory[];
+  CityCategory!:DrCity[];
+  doctors!:Doctor[];
 
-  constructor() { }
+  constructor(private DoctorCatService:CategoryService,private CityCatService:CityCatService) {
+    this.DoctorForm={
+      firstName:'',
+      lastName:'',
+      mobile:0,
+      drCategory:[],
+      drArea:[],
+      drCity:[]
+    }
+   }
 
   ngOnInit(): void {
+    this.DoctorCategory=this.DoctorCatService.AllDoctorCategory();
+    this.CityCategory=this.CityCatService.AllDoctorCity();
   }
 
 }

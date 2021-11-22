af9a7b42d38cc1c72e9ebf8e3833131674501c86
MohammedAshraf <Hamoudy495@gmail.com>
2021-11-22 21:15:30 +0200

    sec update

--- src/app/OnlinePage/online/online.component.ts
+++ src/app/OnlinePage/online/online.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DrCategory } from './../../models/DrCategory.model';
+import { CategoryService } from './../../services/category.service';
 
 @Component({
   selector: 'app-online',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./online.component.scss']
 })
 export class OnlineComponent implements OnInit {
+  DoctorCategory!:DrCategory[];
 
-  constructor() { }
+  constructor( private DoctorCatService:CategoryService,) { }
 
   ngOnInit(): void {
+    this.DoctorCategory = this.DoctorCatService.AllDoctorCategory();
   }
 
 }

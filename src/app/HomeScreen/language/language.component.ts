42ffcd3acf59102c8252652a11633b96bc6e139f
hafsa ahmed <sooahmed83@yahoo.com>
2021-12-05 02:07:55 +0200

    localization with direction but without styling

--- src/app/HomeScreen/language/language.component.ts
+++ src/app/HomeScreen/language/language.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
-import { TranslateService,LangChangeEvent } from '@ngx-translate/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-language',
@@ -9,44 +9,28 @@ import { TranslateService,LangChangeEvent } from '@ngx-translate/core';
 export class LanguageComponent implements OnInit {
   currentLang!:string;
   textDir: string = 'rtl';
-  constructor(public translate:TranslateService,private render:Renderer2) {
+  constructor(public translate:TranslateService) {
     this.currentLang=localStorage.getItem('currentLang') || 'en';
     translate.use(this.currentLang);
-    // this.changeDir();
-  //  document.documentElement.dir=['en,ar'].includes(document.documentElement.lang)?'ltr':'rtl'
-    // if(document.documentElement.lang==="ar"){
-    //   // document.documentElement.dir==="rtl"
-    //   this.render.setAttribute(document.querySelector('html'),'dir','rtl')
-    // }else if(document.documentElement.lang==="en"){
-    //   // document.documentElement.dir==="ltr"
-    //   this.render.setAttribute(document.querySelector('html'),'dir','ltr')
-    // }
-    // this.translate.onLangChange.subscribe((event: LangChangeEvent) =>
-    // {
-    //   if(event.lang == 'ar')
-    //   {
-    //     this.textDir = 'rtl';
-    //   }
-    //   else
-    //   {
-    //     this.textDir = 'ltr';
-    //   }
-    // });
-
+   }
+   activeLang='';
+   changeLang(e:any){
+     const body=document.getElementsByTagName("body");
+     if(e=="ar"){
+       body[0].setAttribute("dir","rtl");
+       this.translate.use(e);
+       localStorage.setItem("currentLang","ar")
+     }else{
+       this.translate.use(e);
+       body[0].setAttribute("dir","ltr");
+       localStorage.setItem("currentLang","en")
+     }
    }
 
   ngOnInit(): void {
-    // this.translate.addLangs(['en', 'ar']);
-    // this.translate.setDefaultLang('en');
   }
   changeCurrentLang(lang:string){
     this.translate.use(lang);
     localStorage.setItem('currentLang',lang)
   }
-  // switchLang(lang: string) {
-  //   const htmlTag =document.documentElement.getElementsByTagName("html")[0] as HTMLHtmlElement;
-  //   htmlTag.dir = lang === "ar" ? "rtl" : "ltr";
-  //   htmlTag.lang = lang;
-  //   this.translate.use(lang);
-  // }
 }

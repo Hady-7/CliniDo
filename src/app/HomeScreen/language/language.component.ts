2a8626b5801006cbb62d74027a2811462c5aef4d
hafsa ahmed <sooahmed83@yahoo.com>
2021-12-07 11:07:19 +0200

    language service

--- src/app/HomeScreen/language/language.component.ts
+++ src/app/HomeScreen/language/language.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/services/language.service';
 
 @Component({
   selector: 'app-language',
@@ -9,28 +10,54 @@ import { TranslateService } from '@ngx-translate/core';
 export class LanguageComponent implements OnInit {
   currentLang!:string;
   textDir: string = 'rtl';
-  constructor(public translate:TranslateService) {
-    this.currentLang=localStorage.getItem('currentLang') || 'en';
-    translate.use(this.currentLang);
+  constructor(public translate:TranslateService,private langSer:LanguageService) {
+    // this.currentLang=localStorage.getItem('currentLang') || 'en';
+    // translate.use(this.currentLang);
    }
-   activeLang='';
    changeLang(e:any){
-     const body=document.getElementsByTagName("body");
+     const html=document.getElementsByTagName("html");
      if(e=="ar"){
-       body[0].setAttribute("dir","rtl");
+       html[0].setAttribute("dir","rtl");
        this.translate.use(e);
        localStorage.setItem("currentLang","ar")
      }else{
        this.translate.use(e);
-       body[0].setAttribute("dir","ltr");
+       html[0].setAttribute("dir","ltr");
        localStorage.setItem("currentLang","en")
      }
+    //  this.langSer.changeCssFile(this.currentLang);
    }
+  //  changeCssFile(lang: String): void {
+  //   let headTag = document.getElementsByTagName("head")[0] as HTMLHeadElement;
+  //   let existingLink = document.getElementById("langCss") as HTMLLinkElement;
+  //   if (lang === 'ar') {
+  //     let bundleName = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.rtl.min.css";
+  //     // note that arrangement  differ in loading
+  //     existingLink.type = "text/css";
+  //     existingLink.crossOrigin = "anonymous";
+  //     existingLink.integrity = "sha384-+qdLaIRZfNu4cVPK/PxJJEy0B0f3Ugv8i482AKY7gwXwhaCroABd086ybrVKTa0q";
+  //     existingLink.href = bundleName;
+  //     console.log("integrity changed")
+
+
+  //   } else {
+  //     let bundleName = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css";
+  //     // note that arrangement  differ in loading
+  //     existingLink.rel = "stylesheet";
+  //     existingLink.type = "text/css";
+  //     existingLink.crossOrigin = "anonymous";
+  //     existingLink.integrity = "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3";
+  //     existingLink.href = bundleName;
+  //     console.log("integrity changed")
+
+  //   }
+  // }
 
   ngOnInit(): void {
+
   }
-  changeCurrentLang(lang:string){
-    this.translate.use(lang);
-    localStorage.setItem('currentLang',lang)
-  }
+  // changeCurrentLang(lang:string){
+  //   this.translate.use(lang);
+  //   localStorage.setItem('currentLang',lang)
+  // }
 }
--- src/app/HomeScreen/static-content/static-content.component.ts
+++ src/app/HomeScreen/static-content/static-content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/services/language.service';
 
 @Component({
   selector: 'app-static-content',
@@ -7,10 +8,14 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./static-content.component.scss']
 })
 export class StaticContentComponent implements OnInit {
+  currentLang:String|undefined;
 
-  constructor(public translate:TranslateService) { }
+  constructor(public translate:TranslateService,private lang:LanguageService) { }
+
 
   ngOnInit(): void {
+    this.currentLang=this.lang.getLanguage();
+    console.log(this.currentLang)
   }
 
 }

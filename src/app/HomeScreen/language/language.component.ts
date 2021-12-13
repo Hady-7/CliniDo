a9365cb7483745052ea814baae92409173c27b95
hafsa ahmed <sooahmed83@yahoo.com>
2021-12-13 14:12:56 +0200

    language buttons

--- src/app/HomeScreen/language/language.component.ts
+++ src/app/HomeScreen/language/language.component.ts
@@ -9,12 +9,12 @@ import { LanguageService } from 'src/app/services/language.service';
 })
 export class LanguageComponent implements OnInit {
   currentLang!:string;
+  imgSrc!:boolean;
   // textDir: string = 'rtl';
   constructor(public translate:TranslateService,public langSer:LanguageService) {
-    // this.currentLang=localStorage.getItem('currentLang') || 'en';
-    // translate.use(this.currentLang);
    }
    changeLang(e:any){
+     this.imgSrc=!this.imgSrc;
      const html=document.getElementsByTagName("html");
      if(e=="ar"){
        html[0].setAttribute("dir","rtl");
@@ -22,40 +22,17 @@ export class LanguageComponent implements OnInit {
        this.translate.use(e);
        localStorage.setItem("currentLang","ar");
        this.langSer.changeCssFile(e);
+      //  this.imgSrc='../../../assets/images/egypt.png';
      }else{
        this.translate.use(e);
        html[0].setAttribute("dir","ltr");
        html[0].classList.add("text-left");
        localStorage.setItem("currentLang","en");
        this.langSer.changeCssFile(e);
+      //  this.imgSrc='../../../assets/images/english.png';
      }
    }
 
-  //  changeCssFile(lang: String): void {
-  //   let headTag = document.getElementsByTagName("head")[0] as HTMLHeadElement;
-  //   let existingLink = document.getElementById("langCss") as HTMLLinkElement;
-  //   if (lang === 'ar') {
-  //     let bundleName = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.rtl.min.css";
-  //     // note that arrangement  differ in loading
-  //     existingLink.type = "text/css";
-  //     existingLink.crossOrigin = "anonymous";
-  //     existingLink.integrity = "sha384-+qdLaIRZfNu4cVPK/PxJJEy0B0f3Ugv8i482AKY7gwXwhaCroABd086ybrVKTa0q";
-  //     existingLink.href = bundleName;
-  //     console.log("integrity changed")
-
-
-  //   } else {
-  //     let bundleName = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css";
-  //     // note that arrangement  differ in loading
-  //     existingLink.rel = "stylesheet";
-  //     existingLink.type = "text/css";
-  //     existingLink.crossOrigin = "anonymous";
-  //     existingLink.integrity = "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3";
-  //     existingLink.href = bundleName;
-  //     console.log("integrity changed")
-
-  //   }
-  // }
 
   ngOnInit(): void {
 

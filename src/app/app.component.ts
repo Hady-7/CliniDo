2f34f5332f8f6cbcc9871253832ba952daeb4be9
hafsa ahmed <sooahmed83@yahoo.com>
2021-12-13 21:56:57 +0200

    Styling without header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,31 +11,6 @@ export class AppComponent {
   constructor(){
 
   }
-  changeCssFile(lang: String): void {
-    let headTag = document.getElementsByTagName("head")[0] as HTMLHeadElement;
-    let existingLink = document.getElementById("langCss") as HTMLLinkElement;
-    if (lang === 'ar') {
-      let bundleName = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.rtl.min.css";
-      // note that arrangement  differ in loading
-      existingLink.type = "text/css";
-      existingLink.crossOrigin = "anonymous";
-      existingLink.integrity = "sha384-+qdLaIRZfNu4cVPK/PxJJEy0B0f3Ugv8i482AKY7gwXwhaCroABd086ybrVKTa0q";
-      existingLink.href = bundleName;
-      console.log("integrity changed")
-
-
-    } else {
-      let bundleName = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css";
-      // note that arrangement  differ in loading
-      existingLink.rel = "stylesheet";
-      existingLink.type = "text/css";
-      existingLink.crossOrigin = "anonymous";
-      existingLink.integrity = "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3";
-      existingLink.href = bundleName;
-      console.log("integrity changed")
-
-    }
-  }
 
 
 }

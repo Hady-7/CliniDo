eee5b19f99b6aa529871cf5cdb2ff908b87ee743
hafsa ahmed <sooahmed83@yahoo.com>
2021-12-19 23:28:53 +0200

    chatBo without messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent {
   constructor(){
 
   }
+  ngOnInit() {
+    (function(d, m){
+        var kommunicateSettings = {"appId":"37df7e5fccba77352ff493a6987971815","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        (window as any).kommunicate = m; m._globals = kommunicateSettings;
+    })(document, (window as any).kommunicate || {});
+
+  }
 
 
 }
